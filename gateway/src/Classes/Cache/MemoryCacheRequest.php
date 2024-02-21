55d6d02e3037ff95249d086b554bbd74f10231da
SidAlex <palach.orl@mail.ru>
2024-02-21 12:06:31 +0300

    add validate cache method

--- gateway/src/Classes/Cache/MemoryCacheRequest.php
+++ gateway/src/Classes/Cache/MemoryCacheRequest.php
@@ -4,11 +4,14 @@ namespace Sidalex\Gateway\Classes\Cache;
 
 class MemoryCacheRequest implements CacheRequestInterface
 {
+    /**
+     * @var array<mixed>
+     */
     private array $repository = [];
 
-    public function setCache(string $key, $value,int $ttl = 600): void
+    public function setCache(string $key, mixed $value, int $ttl = 600): void
     {
-        if ($ttl==0){
+        if ($ttl == 0) {
             $ttl = 9999999;
         }
         $this->repository[$key]['value'] = $value;
@@ -16,7 +19,7 @@ class MemoryCacheRequest implements CacheRequestInterface
 
     }
 
-    public function getCache(string $key)
+    public function getCache(string $key): mixed
     {
         return $this->repository[$key]['value'];
     }
@@ -31,5 +34,21 @@ class MemoryCacheRequest implements CacheRequestInterface
             }
         }
         return false;
+    }
+
+    public function validateCache(): void
+    {
+        $newRepository = [];
+
+        $newRepository = array_filter(
+            $this->repository,
+            function ($value) {
+                if ($value['time'] > time()) {
+                    return $value;
+                }
+            }
+        );
+        unset($this->repository);
+        $this->repository = $newRepository;
     }
 }
