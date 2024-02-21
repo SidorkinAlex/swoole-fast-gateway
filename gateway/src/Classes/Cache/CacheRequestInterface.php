4df904021b0740b6b93388d37ff383d7d51b7fe7
SidAlex <palach.orl@mail.ru>
2024-02-21 12:05:05 +0300

    update typehint

--- gateway/src/Classes/Cache/CacheRequestInterface.php
+++ gateway/src/Classes/Cache/CacheRequestInterface.php
@@ -4,9 +4,9 @@ namespace Sidalex\Gateway\Classes\Cache;
 
 interface CacheRequestInterface
 {
-    public function setCache(string $key, $value,int $ttl): void;
+    public function setCache(string $key, mixed $value, int $ttl): void;
 
-    public function getCache(string $key);
+    public function getCache(string $key): mixed;
 
     public function hasKey(string $key): bool;
 }
