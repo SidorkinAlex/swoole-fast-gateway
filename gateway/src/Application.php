cfd386789ee33fcb1a6aa195b2d08bf39c9d3797
SidAlex <palach.orl@mail.ru>
2024-02-24 00:24:23 +0300

    add init config metchod

--- gateway/src/Application.php
+++ gateway/src/Application.php
@@ -4,14 +4,16 @@ namespace Sidalex\Gateway;
 
 use HaydenPierce\ClassFinder\ClassFinder;
 use Sidalex\Gateway\Classes\Cache\CacheRequestCollector;
+use Sidalex\Gateway\Classes\Cache\ResponseCache;
 use Sidalex\Gateway\Classes\Client\Requester;
 use Sidalex\Gateway\Classes\Collectors\SwooleRequestMutationCollector;
+use Sidalex\Gateway\Classes\Config\ConfigException;
+use Sidalex\Gateway\Classes\Config\ConfigValidator;
 use Sidalex\Gateway\Classes\CoreLogicExecutors\LogicExecutorInterface;
 use stdClass;
 
 class Application
 {
-    private \Swoole\Tables $tables;
     private stdClass $config;
 
     private CacheRequestCollector $repositoryCache;
@@ -19,8 +21,7 @@ class Application
 
     public function __construct()
     {
-        $this->config = json_decode(file_get_contents(__DIR__ . "/../config.json"));
-        $this->add2Config();
+        $this->initConfig();
         $this->repositoryCache = new CacheRequestCollector($this->config);
     }
 
