eabc7fa319a90a39fe1b482d9c0811ff30dd4d37
SidAlex <palach.orl@mail.ru>
2024-02-24 00:30:11 +0300

    add method config init

--- gateway/src/Application.php
+++ gateway/src/Application.php
@@ -25,5 +25,63 @@ class Application
         $this->repositoryCache = new CacheRequestCollector($this->config);
     }
 
+    private function initConfig(): void
+    {
+        $content = file_get_contents(__DIR__ . "/../config.json");
+        if($content === false){
+            echo "config.json not exist or is not readable";
+            exit(1);
+        }
+        $config_body = json_decode($content);
+        unset($content);
+        if($config_body === false){
+         echo "config.json is not set or is not valid JSON";
+            exit(1);
+        }
+        $this->config=$config_body;
+        unset($config_body);
+        try {
+            $this->config = ConfigValidator::Validate($this->config);
+        } catch (ConfigException $e) {
+            echo $e->getMessage() . " error code is" . $e->getCode();
+            exit(1);
+        }
+        $this->add2Config();
+    }
+    private function add2Config(): void
+    {
+        $namespace = [
+            'AfterSendingLogicExecutors' => 'Sidalex\Gateway\Classes\AfterSendingLogicExecutors',
+            'BeforeSendingLogicExecutors' => 'Sidalex\Gateway\Classes\BeforeSendingLogicExecutors',
+        ];
+        try {
+            $classes = ClassFinder::getClassesInNamespace($namespace['AfterSendingLogicExecutors']);
+            foreach ($classes as $class) {
+                $object = new $class();
+                if ($object instanceof LogicExecutorInterface) {
+                    $this->config->AfterSendingLogicExecutors[] = $class;
+                }
+                unset($object);
+            }
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+            exit(1);
+        }
+
+        try {
+            $classes = ClassFinder::getClassesInNamespace($namespace['BeforeSendingLogicExecutors']);
+            foreach ($classes as $class) {
+                $object = new $class();
+                if ($object instanceof LogicExecutorInterface) {
+                    $this->config->BeforeSendingLogicExecutors[] = $class;
+                }
+                unset($object);
+            }
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+            exit(1);
+        }
+    }
+
 
 }
