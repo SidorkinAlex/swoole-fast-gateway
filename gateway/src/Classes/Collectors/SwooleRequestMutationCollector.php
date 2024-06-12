f5bf9d1f683ef5bd173d48fc643c4a5b063ec9c1
SidorkinAlex <palach.orl@mail.ru>
2024-06-12 14:01:23 +0300

    add Swooly Request Mutation Collector

---
+++ gateway/src/Classes/Collectors/SwooleRequestMutationCollector.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sidalex\Gateway\Classes\Collectors;
+
+class SwooleRequestMutationCollector extends AbstractCollector implements CollectorInterface
+{
+    static string $CONFIG_TITLE = "PsrRequestMutationClass";
+
+}
