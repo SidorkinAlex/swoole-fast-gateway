8fb61bb3902547729bb2cd011fe4fd36a8b6658c
SidorkinAlex <palach.orl@mail.ru>
2024-06-12 14:07:06 +0300

    add proxy types

---
+++ gateway/src/Classes/Config/EndpointProxyPoliticTypes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sidalex\Gateway\Classes\Config;
+
+class EndpointProxyPoliticTypes
+{
+    const TYPES = [
+        "proxy_only_to_host",
+        "uri_whitelist",
+        "uri_blacklist",
+    ];
+
+}
