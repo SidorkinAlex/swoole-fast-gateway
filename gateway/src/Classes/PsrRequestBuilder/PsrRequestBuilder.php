31e952ab540988b79b790dfe50ce73d9a137367b
SidorkinAlex <palach.orl@mail.ru>
2024-06-26 16:05:35 +0300

    updateing from PsrRequest

--- gateway/src/Classes/PsrRequestBuilder/PsrRequestBuilder.php
+++ gateway/src/Classes/PsrRequestBuilder/PsrRequestBuilder.php
@@ -7,18 +7,24 @@ use Swoole\Http\Request;
 
 class PsrRequestBuilder
 {
-    public function buildRequest(Request $swooleRequest,\stdClass $config): PsrRequest
+    public function buildRequest(Request $swooleRequest, \stdClass $config): PsrRequest
     {
         $host = (new HostBuilder)->hostBuild($config);
-        $uri = $host.$swooleRequest->server['request_uri'];
-        if (isset($swooleRequest->server['query_string']) && !empty($swooleRequest->server['query_string'])){
-            $uri.="?".$swooleRequest->server['query_string'];
+        $uri = $host . $swooleRequest->server['request_uri'];
+        if (isset($swooleRequest->server['query_string']) && !empty($swooleRequest->server['query_string'])) {
+            $uri .= "?" . $swooleRequest->server['query_string'];
         }
-        unset( $swooleRequest->header['host']);
-        if (is_string($swooleRequest->getContent())){
-            return new PsrRequest($swooleRequest->getMethod(),$uri ,$swooleRequest->header,$swooleRequest->getContent());
+        unset($swooleRequest->header['host']);
+        $header = $swooleRequest->header;
+        $cookie_string = '';
+        foreach ($swooleRequest->cookie as $key => $value) {
+            $cookie_string .= $key . '=' . $value . ';';
         }
-        return new PsrRequest($swooleRequest->getMethod(),$uri ,$swooleRequest->header,'');
+        $header['Cookie'] = $cookie_string;
+        if (is_string($swooleRequest->getContent())) {
+            return new PsrRequest($swooleRequest->getMethod(), $uri, $header, $swooleRequest->getContent());
+        }
+        return new PsrRequest($swooleRequest->getMethod(), $uri, $swooleRequest->header, '');
     }
 
 }
